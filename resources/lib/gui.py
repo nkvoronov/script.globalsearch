60e9547640f05898ebee4e01aaf0280863dd779a
ronie <ronie@users.noreply.github.com>
2018-04-12 22:46:24 +0200

    favourites fix

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -68,11 +68,10 @@ class GUI(xbmcgui.WindowXML):
         json_response = json.loads(json_query)
         if 'result' in json_response and json_response['result'] != None and 'favourites' in json_response['result'] and json_response['result']['favourites'] != None:
             for item in json_response['result']['favourites']:
-                print str(item)
                 if 'path' in item:
                     self.favourites.append(item['path'])
-                else:
-                    self.favourites.append(item['windowparameter'])      
+                elif 'windowparameter' in item:
+                    self.favourites.append(item['windowparameter'])
 
     def _reset_variables(self):
         self.focusset= 'false'

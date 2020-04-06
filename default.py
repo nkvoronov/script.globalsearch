2a78e1c783735cd9ef955229cc24c3faa395bc63
ronie <ronie@kodi.tv>
2020-04-06 02:15:04 +0200

    cleanup

--- default.py
+++ default.py
@@ -1,27 +1,24 @@
-import os
 import sys
 from urllib.parse import unquote_plus
 import xbmc
 import xbmcaddon
 
-ADDON = xbmcaddon.Addon()
-LANGUAGE = ADDON.getLocalizedString
-CWD = ADDON.getAddonInfo('path')
+LANGUAGE = xbmcaddon.Addon().getLocalizedString
+CWD = xbmcaddon.Addon().getAddonInfo('path')
 
 if (__name__ == '__main__'):
     try:
         params = dict(arg.split('=') for arg in sys.argv[ 1 ].split('&'))
     except:
         params = {}
-    searchstring = params.get('searchstring','')
-    searchstring = unquote_plus(searchstring)
-    if searchstring == '':
+    searchstring = unquote_plus(params.get('searchstring',''))
+    if searchstring:
+        del params['searchstring']
+    else:
         keyboard = xbmc.Keyboard('', LANGUAGE(32101), False)
         keyboard.doModal()
         if (keyboard.isConfirmed()):
             searchstring = keyboard.getText()
-    else:
-        del params['searchstring']
     if searchstring:
         from lib import gui
         ui = gui.GUI('script-globalsearch.xml', CWD, 'default', '1080i', True, searchstring=searchstring, params=params)
